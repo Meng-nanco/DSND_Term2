81b8ec81c498f6abeb4a35ade55629a0dbd19f08
Meng Tan <nanco104@gmail.com>
2021-05-16 15:37:40 +0100

    Create and add Binomial Class into distributions package

--- lessons/ObjectOrientedProgramming/IdeFiles/4a_binomial_package/distributions/Binomialdistribution.py
+++ lessons/ObjectOrientedProgramming/IdeFiles/4a_binomial_package/distributions/Binomialdistribution.py
@@ -36,11 +36,11 @@ class Binomial(Distribution):
         # TODO: Now that you know p and n, you can calculate the mean and standard deviation
         #       You can use the calculate_mean() and calculate_stdev() methods defined below along
         #       with the __init__ function from the Distribution class
-        def __init__(self, prob=.5, size=20):
-            self.p = prob
-            self.n = size
+    def __init__(self, prob=.5, size=20):
+        self.p = prob
+        self.n = size
 
-            Distribution.__init__(self, self.calculate_mean(), self.calculate_stdev())
+        Distribution.__init__(self, self.calculate_mean(), self.calculate_stdev())
 
     # TODO: write a method calculate_mean() according to the specifications below
     def calculate_mean(self):
@@ -176,6 +176,8 @@ class Binomial(Distribution):
         plt.xlabel("Outcomes")
         plt.ylabel("Probability")
         plt.title("Probability of Outcomes")
+
+        plt.show()
 
         return x, y
 
